66fd9556edf3a82019738dd4aef004a21492b8de
sina0101 <jafarisina52@gmail.com>
2025-04-22 17:26:21 +0330

    write User model coustom

--- account_app/admin.py
+++ account_app/admin.py
@@ -1,3 +1,99 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 
-# Register your models here.
+from .models import MyUser
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = [
+            'email', 'date_of_birth', 'first_name', 'last_name', 'username',
+            'nickname', 'phone_number'
+        ]
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+        if not password1:
+            raise ValidationError("Password cannot be empty.")
+        validate_password(password1)
+        return password1
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords do not match.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        help_text="Raw passwords are not stored, so there is no way to see "
+                  "this user's password, but you can change the password "
+                  "using <a href=\"../password/\">this form</a>."
+    )
+
+    class Meta:
+        model = MyUser
+        fields = [
+            "email", "password", "date_of_birth",
+            "first_name", "last_name", "nickname",
+            "username", "phone_number",
+            "is_active", "is_admin"
+        ]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ["email", "first_name", "last_name", "is_admin", "is_active"]
+    list_filter = ["is_admin", "is_active"]
+    
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {
+            "fields": [
+                "first_name", "last_name", "nickname", "date_of_birth",
+                "username", "phone_number"
+            ]
+        }),
+        ("Permissions", {"fields": ["is_admin", "is_active"]}),
+    ]
+    
+    add_fieldsets = [
+        (None, {
+            "classes": ["wide"],
+            "fields": [
+                "email", "date_of_birth", "first_name", "last_name",
+                "nickname", "username", "phone_number",
+                "password1", "password2"
+            ],
+        }),
+    ]
+
+    search_fields = ["email", "first_name", "last_name", "username"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(MyUser, UserAdmin)
+admin.site.unregister(Group)
--- account_app/models.py
+++ account_app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email, date_of_birth, password, **extra_fields)
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    date_of_birth = models.DateField()
+
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    nickname = models.CharField(max_length=30, blank=True)
+    username = models.CharField(max_length=20, blank=True)
+    
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
